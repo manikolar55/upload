49c35a8d3f36307d1061de11a739cc006ee8ff09
manikolar55 <82044523+manikolar55@users.noreply.github.com>
2021-04-07 19:41:31 +0500

    second commit

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["finalproject4900.herokuapp.com","localhost"]
+ALLOWED_HOSTS = ["fyp490.herokuapp.com/","localhost"]
 
 
 # Application definition
